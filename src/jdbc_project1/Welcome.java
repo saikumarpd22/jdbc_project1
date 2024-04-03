8741c98a14ce69fb963e7c6a759fb0c767e1747d
saikumarpd22 <saikumarpd22@gmail.com>
2024-04-03 18:14:15 +0530

    added new file

--- src/jdbc_project1/Welcome.java
+++ src/jdbc_project1/Welcome.java
@@ -1,13 +1,10 @@
 package jdbc_project1;
 
-public class Hello {
-
-	
+public class Welcome {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Java");
-		System.out.println("Hii java");
+		System.out.println(" ");
 
 	}
 
