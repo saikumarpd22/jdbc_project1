635d18b66b02c1e44462c7976fbec6fe6da6f5bd
saikumarpd22 <saikumarpd22@gmail.com>
2024-04-03 17:39:04 +0530

    first commit

---
+++ src/jdbc_project1/Hello.java
@@ -0,0 +1,13 @@
+package jdbc_project1;
+
+public class Hello {
+
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Java");
+
+	}
+
+}
